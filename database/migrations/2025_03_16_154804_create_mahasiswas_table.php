25cf898e8bbede9a785328888e35353f156fbb5d
zakiluwak <l200220208@student.ums.ac.id>
2025-03-16 22:53:57 +0700

    tabel mahasiswa

---
+++ database/migrations/2025_03_16_154804_create_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nim')->primary();
+            $table->string('nama_mahasiswa');
+            $table->date('tanggal_lahir');
+            $table->string('alamat_ms');
+            $table->string('prodi_ms');
+            $table->string('email_ms')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
