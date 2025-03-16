9c1cbd7bf7a0b5ecbd21897751e149b4bbf65efa
Inandra Asha Fardhana <inandraashafardhana@192.168.1.31>
2025-03-16 23:34:50 +0700

    model selesai

---
+++ database/migrations/2025_03_16_162515_create_log_pt_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('log_pt', function (Blueprint $table) {
+            $table->id('idlog_pt');
+            $table->unsignedBigInteger('pengumpulan_tg_id_pt');
+            $table->timestamp('timestamp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('log_pt');
+    }
+};
