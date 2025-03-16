8c3a9c43f858b3ff655d15ba16d35ecb7006b9cb
Inandra Asha Fardhana <inandraashafardhana@192.168.1.31>
2025-03-16 23:08:31 +0700

    tambah dikit

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('role');
             $table->string('username')->unique();
             $table->string('password');
             $table->string('name');
