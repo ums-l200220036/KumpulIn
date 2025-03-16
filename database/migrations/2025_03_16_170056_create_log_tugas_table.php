eec4792c46e1f45de84f56fcec00330c0abd1a51
zakiluwak <l200220208@student.ums.ac.id>
2025-03-17 00:09:53 +0700

    tugas

---
+++ database/migrations/2025_03_16_170056_create_log_tugas_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('log_tugas', function (Blueprint $table) {
+            $table->id('idlog_tugas');
+            $table->string('tugas_id_tugas');
+            $table->timestamps();
+            $table->foreign('tugas_id_tugas')->references('id_tugas')->on('tugas')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('log_tugas');
+    }
+};
