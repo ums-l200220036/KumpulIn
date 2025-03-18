321e6f2ff1ee1624c1bd7cf19dac6fc4d8940fd9
Inandra Asha Fardhana <inandraashafardhana@192.168.1.2>
2025-03-18 15:18:42 +0700

    penambahan kolom db

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -9,7 +9,7 @@ class Dosen extends Authenticatable
 {
     protected $table = 'dosen';
     protected $primaryKey = 'nidn';
-    protected $fillable = ['nidn', 'nama', 'password_ds'];
+    protected $fillable = ['nidn', 'nama', 'mata_kuliah','password_ds'];
     protected $hidden = ['password_ds'];
 
     public function getAuthPassword()
--- database/migrations/2025_03_16_154804_create_mahasiswa_table.php
+++ database/migrations/2025_03_16_154804_create_mahasiswa_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('mahasiswa', function (Blueprint $table) {
             $table->string('nim')->primary();
             $table->string('nama');
+            $table->string('semester');
             $table->string('password_ms');
             $table->timestamps();
         });
